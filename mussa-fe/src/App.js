144253b1e302df5404c65dd674767ac42dfa7226
ya-suke <fredluki100@gmail.com>
2021-10-15 21:10:07 +0300

    fix mobile resize

--- mussa-fe/src/App.js
+++ mussa-fe/src/App.js
@@ -15,13 +15,13 @@ function App() {
   );
 }
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-//let vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty("--vh", `${vh}px`);
-// window.addEventListener("resize", () => {
-// We execute the same script as before
-//  let vh = window.innerHeight * 0.01;
-//  document.documentElement.style.setProperty("--vh", `${vh}px`);
-// });
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
 
 export default App;
--- mussa-fe/src/components/center/centerright/Centerright.js
+++ mussa-fe/src/components/center/centerright/Centerright.js
@@ -113,3 +113,11 @@ export default function Centerright() {
     </div>
   );
 }
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
